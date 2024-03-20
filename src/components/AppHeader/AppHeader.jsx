493a65028f4880f663be4b6e26c039695e5b2493
kiselev <Raikoz@yandex.ru>
2024-03-20 09:00:59 +0700

    AppHeader переделал на функциональный

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -3,34 +3,31 @@ import { BurgerIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 import { ListIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 import { ProfileIcon } from "@ya.praktikum/react-developer-burger-ui-components";
 import AppHeaderStyles from "./AppHeader.module.css";
-import React from "react";
 
-export default class AppHeader extends React.Component {
-  render() {
-    return (
-      <header className={AppHeaderStyles.header}>
-        <div className={AppHeaderStyles.menu}>
-          <div className={AppHeaderStyles.leftButtons}>
-            <div className={AppHeaderStyles.menuItem}>
-              <BurgerIcon type="primary"></BurgerIcon>
-              <p className={"text text_type_main-small"}>Конструктор</p>
-            </div>
-            <div className={AppHeaderStyles.menuItem}>
-              <ListIcon type="secondary"></ListIcon>
-              <p className={"text_color_inactive"}>Лента заказов</p>
-            </div>
+export default function AppHeader() {
+  return (
+    <header className={AppHeaderStyles.header}>
+      <div className={AppHeaderStyles.menu}>
+        <div className={AppHeaderStyles.leftButtons}>
+          <div className={AppHeaderStyles.menuItem}>
+            <BurgerIcon type="primary"></BurgerIcon>
+            <p className={"text text_type_main-small"}>Конструктор</p>
           </div>
-          <div className={AppHeaderStyles.logo}>
-            <Logo />
-          </div>
-          <div className={AppHeaderStyles.rightButtons}>
-            <div className={AppHeaderStyles.menuItem}>
-              <ProfileIcon type="secondary"></ProfileIcon>
-              <p className={"text_color_inactive"}>Личный кабинет</p>
-            </div>
+          <div className={AppHeaderStyles.menuItem}>
+            <ListIcon type="secondary"></ListIcon>
+            <p className={"text_color_inactive"}>Лента заказов</p>
           </div>
         </div>
-      </header>
-    );
-  }
+        <div className={AppHeaderStyles.logo}>
+          <Logo />
+        </div>
+        <div className={AppHeaderStyles.rightButtons}>
+          <div className={AppHeaderStyles.menuItem}>
+            <ProfileIcon type="secondary"></ProfileIcon>
+            <p className={"text_color_inactive"}>Личный кабинет</p>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 }
